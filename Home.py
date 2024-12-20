5448f71a9711829c82ba8e8f55dd59691b9eb132
Sudeshna Kundu <140314320+iamsudeshna@users.noreply.github.com>
2024-12-20 16:39:16 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,12 +8,12 @@ st.set_page_config(
     layout = 'wide')
 
 st.title("About the Project")
-st.write("We are analysing the app Instabridge for our usecase. Its an app which locates and provides free wifi. We are trying to analyse from the past ratings and reviews of Instabridge itself, and trying interpret the positive and negative sentiments of the reviews")
+st.write("We are analysing an App/Game for our usecase. Its an app which locates and provides free wifi. We are trying to analyse from the past ratings and reviews of the target app itself, and trying interpret the positive and negative sentiments of the reviews")
 st.write("We are also trying to generate better short descriptions from the positive sentiments of reviews and Past AB Tests Data for short descriptions")
-st.write("We are also having few competitors of Instabridge, which are, Wifi Map, WifiMan. From the reviews of these competitor apps we will analyse the sentiments and infer how and why these are better or worse than Instabridge and how they are in competition with Instabridge")
+st.write("We are also having few competitors of our target App/Game. From the reviews of these competitor apps we will analyse the sentiments and infer how and why these are better or worse than our App and how they are in competition with the App/Game")
 # Create a dropdown menu
-apps = ["InstaBridge"]
-Competitors = ["Wifi Map", "WifiMan"]
+apps = ["App_InstaBridge-123#"]
+Competitors = ["Wifi Map - 101#", "WifiMan - 102#"]
 language = ["English"]
 selected_apps = st.selectbox("Choose the app for analysis:", apps)
 selected_competitor = st.selectbox("Choose the competitor app for analysis:", Competitors)
